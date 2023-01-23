3bbe58b3fec72a0ff8d8247898aaada72cbe4881
Ismael <ismaelma1996@gmail.com>
2023-01-23 21:16:29 +0100

    Initial commit, Comprobar número funcionando con Strings

---
+++ src/menuprincipal/MenuPrincipal.java
@@ -0,0 +1,2 @@
+package menuprincipal;public class MenuPrincipal {
+}
---
+++ src/numero/Numero.java
@@ -0,0 +1,26 @@
+public class Numero
+{
+    private String numero;
+    private int premio;
+
+    public Numero(String numero, int premio) {
+        this.numero = numero;
+        this.premio = premio;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public int getPremio() {
+        return premio;
+    }
+
+    public void setPremio(int premio) {
+        this.premio = premio;
+    }
+}
